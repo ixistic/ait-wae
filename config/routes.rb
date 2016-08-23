0132f482a6a6c61b6ab991ad3b9a4993a04ecfda
ixistic <teeravipark@gmail.com>
2016-08-23 14:29:29 +0700

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'basic1/index'
 
-  root 'basic1#index'
+  # root 'basic1#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
