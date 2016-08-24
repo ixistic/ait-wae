317829c35034c9240da77a59882fd7051d93cc9f
ixistic <teeravipark@gmail.com>
2016-08-24 17:14:52 +0700

    refactor

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -1,10 +1,5 @@
 class BasicsController < ApplicationController
   def index
-    # For production server
-    # @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending',:proxy=>'http://192.41.170.23:3128'), nil, Encoding::UTF_8.to_s)
-    #           .css('li.expanded-shelf-content-item-wrapper')
-
-    # For development server
     @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'), nil, Encoding::UTF_8.to_s)
               .css('li.expanded-shelf-content-item-wrapper')
 
