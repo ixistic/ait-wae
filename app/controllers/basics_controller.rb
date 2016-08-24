3bb26df2be42090bde9b861f6cfd0b8b414a1f47
ixistic <teeravipark@gmail.com>
2016-08-24 15:12:30 +0700

    refactor

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -1,22 +1,22 @@
 class BasicsController < ApplicationController
   def index
     # For production server
-    doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending',:proxy=>'http://192.41.170.23:3128'), nil, Encoding::UTF_8.to_s)
+    @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending',:proxy=>'http://192.41.170.23:3128'), nil, Encoding::UTF_8.to_s)
               .css('li.expanded-shelf-content-item-wrapper')
 
     # For development server
-    # doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'), nil, Encoding::UTF_8.to_s)
+    # @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'), nil, Encoding::UTF_8.to_s)
     #           .css('li.expanded-shelf-content-item-wrapper')
 
 
-    @result = doc.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link')
+    @result = @doc.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link')
                   .take(10)
                   .map {|video|
-                      video_id = video['href'].split('=')[1]
+                      @video_id = video['href'].split('=')[1]
                       [
                         video['title'],
                        'http://www.youtube.com'+video['href'],
-                       'http://img.youtube.com/vi/'+video_id+'/default.jpg'
+                       'http://img.youtube.com/vi/'+@video_id+'/default.jpg'
                       ]}
 
   end
