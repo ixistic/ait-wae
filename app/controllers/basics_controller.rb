3ad2358d17ee1ba21dcca05ef3be585dc2663eba
ixistic <teeravipark@gmail.com>
2016-08-24 17:11:24 +0700

    try proxy

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -1,12 +1,12 @@
 class BasicsController < ApplicationController
   def index
     # For production server
-    @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending',:proxy=>'http://192.41.170.23:3128'), nil, Encoding::UTF_8.to_s)
-              .css('li.expanded-shelf-content-item-wrapper')
+    # @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending',:proxy=>'http://192.41.170.23:3128'), nil, Encoding::UTF_8.to_s)
+    #           .css('li.expanded-shelf-content-item-wrapper')
 
     # For development server
-    # @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'), nil, Encoding::UTF_8.to_s)
-    #           .css('li.expanded-shelf-content-item-wrapper')
+    @doc = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'), nil, Encoding::UTF_8.to_s)
+              .css('li.expanded-shelf-content-item-wrapper')
 
 
     @result = @doc.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link')
