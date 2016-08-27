9f84d174b65dc89f3a849804d9b025f419f3166c
al-fian <alfian.alfian@cs.ait.ac.th>
2016-08-27 21:12:05 +0700

    Solving the link problem with route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'basics', to: 'basics#index'
-  get 'basics/youtube-top-ten'
   get 'basics/exception', to: 'basics#exception'
   get 'basics/exception-explanation', to: 'basics#exception_explanation'
+  get 'basics/youtube-top-ten', to: 'basics#youtube_top_ten'
 
   root 'home#index'
+  root 'basics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
