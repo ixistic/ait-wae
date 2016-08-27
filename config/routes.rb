307b142d575e1eff62cd0ade64c68e6288985f84
al-fian <alfian.alfian@cs.ait.ac.th>
2016-08-27 16:38:08 +0700

    Simplying the homepage by placing only 2 lists there

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'basics', to: 'basics#index'
   get 'basics/exception'
+  get 'basics/exception-explanation'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
