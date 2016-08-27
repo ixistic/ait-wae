d285763d04f273ca2d4aea1e17ca8208b63c28da
al-fian <alfian.alfian@cs.ait.ac.th>
2016-08-27 17:51:58 +0700

    Adding basics#exception to the route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'basics', to: 'basics#index'
-  get 'basics/exception'
+  get 'basics/exception', to: 'basics#exception'
   get 'basics/exception-explanation', to: 'basics#exception_explanation'
 
   root 'home#index'
